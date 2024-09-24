e990f59f19171e9fea3ef4c28da164573d85063a
Pratik Sah <pratik.sah@razorpay.com>
2024-09-25 02:01:02 +0530

    Added utility function for getting the pincode data

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-module.exports = require("./pincode.json");
+const pincode = require('./pincode.json');
+
+/**
+ *
+ * @param {string} pin Enter any pincode to get city data. Example: 854301
+ * @returns {object} Returns city data of the given pincode.
+ */
+function getCityDataByPinCode(pin) {
+	return pincode[pin];
+}
+
+module.exports = { getCityDataByPinCode };
+module.exports = pincode;
