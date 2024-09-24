41b348662ffe146b0b05dc8533edf359f6d34aab
Pratik Sah <pratik.sah@razorpay.com>
2024-09-25 02:06:23 +0530

    Updated json file with proper formatting

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-const pincode = require('./pincode.json');
-
-/**
- *
- * @param {string} pin Enter any pincode to get city data. Example: 854301
- * @returns {object} Returns city data of the given pincode.
- */
-function getCityDataByPinCode(pin) {
-	return pincode[pin];
-}
-
-module.exports = { getCityDataByPinCode };
-module.exports = pincode;
+module.exports = require("./pincode.json");
